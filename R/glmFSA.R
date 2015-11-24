9e3d22230e169a84ba4065cd67ea60e55c42e53f
sjanse <sarah.janse@uky.edu>
2015-11-24 13:11:23 -0500

    output table update
    updated the output table to contain the p-values

--- R/glmFSA.R
+++ R/glmFSA.R
@@ -35,18 +35,18 @@ glmFSA=function(formula,data,fixvar=NULL,quad=F,m=2,numrs=1,save_solutions=T,fam
   startvar<-get.vars()
   fit=glm(formula,data=data,family=fam,...)
   ypos<-which(colnames(data)==lhsvar)
-
+  
   xdata<-data[,-ypos]
   ydata<-data[,ypos]
   newdata<-data.frame(cbind(ydata,xdata))
   fixpos<-which(colnames(xdata) %in% fixvar)
   if(length(fixpos)==0){fixpos=NULL}
-
+  
   history<-matrix(rep(NA,numrs*(2*m+2)),ncol=(2*m+2))
   history[,1:m]<-rstart(m=m,nvars=(dim(newdata)[2]-1),numrs=numrs)
   curpos<-which(colnames(xdata) %in% startvar[-1])
   if(length(curpos)!=0){history<-rbind(c(curpos,rep(NA,length(curpos)+2)),history)}
-
+  
   fsa<-function(i,history,...){
     cur<-history[i,1:m]
     last<-rep(NA,m)
@@ -60,42 +60,43 @@ glmFSA=function(formula,data,fixvar=NULL,quad=F,m=2,numrs=1,save_solutions=T,fam
         anv<-anova(fit,test="Chisq")
         c(summary(fit)$aic,anv[dim(anv)[1],dim(anv)[2]])
       },mc.cores=cores)
-
+      
       tmp<-matrix(unlist(tmp),byrow=T,ncol=2)
       cur<-moves[,which.min(tmp[,2])]
-      r.sq<-tmp[which.min(tmp[,2]),2]
+      pval<-tmp[which.min(tmp[,2]),2]
       numswap<-numswap+1
     }
     history[i,(1+m):(2*m)]<-cur
-    history[i,(dim(history)[2]-1)]<-r.sq
+    history[i,(dim(history)[2]-1)]<-pval
     history[i,(dim(history)[2])]<-numswap-1
     return(history[i,])
   }
   solutions<-matrix(unlist(lapply(1:numrs,FUN =function(i) fsa(i,history))),ncol=dim(history)[2],byrow = T)
   solutions[,1:(2*m)]<-matrix(colnames(newdata)[c(solutions[,1:(2*m)]+1)],ncol=(2*m))
   solutions<-data.frame(solutions)
-  colnames(solutions)[dim(solutions)[2]:(dim(solutions)[2]-1)]=c("swaps","r.sq")
+  colnames(solutions)[dim(solutions)[2]:(dim(solutions)[2]-1)]=c("swaps","pval")
   colnames(solutions)[1:m]=paste("start",1:m,sep=".")
   colnames(solutions)[(m+1):(m*2)]=paste("best",1:m,sep=".")
-  solutions$r.sq<-as.numeric(levels(solutions$r.sq))[solutions$r.sq]
+  solutions$pval<-as.numeric(levels(solutions$pval))[solutions$pval]
   solutions$swaps<-as.numeric(levels(solutions$swaps))[solutions$swaps]
   if(length(fixvar)!=0){solutions<-data.frame(fixvar=matrix(rep(x=fixvar,dim(solutions)[1]),nrow=dim(solutions)[1],byrow=T),solutions)}
   if(save_solutions==T){write.csv(solutions,paste0(getwd(),"/FSAsolutions",".csv"))}
   solutions<<-solutions
-  a<-solutions[,(length(fixvar)+m+1):(length(fixvar)+m+1+m-1)]
+  a<-solutions[,(length(fixvar)+m+1):(length(fixvar)+m+1+m)]
   b<-unique(t(apply(a,sort,MARGIN = 1)),MARGIN = 1)
   a<-t(apply(a,sort,MARGIN = 1))
   c<-cbind(b,0)
   for(i in 1:dim(b)[1]){
     for(j in 1:dim(a)[1]){
-      c[i,(m+1)]<-sum(as.numeric(c[i,(m+1)])+as.numeric(identical(a[j,],b[i,])))
+      c[i,(m+2)]<-sum(as.numeric(c[i,(m+2)])+as.numeric(identical(a[j,],b[i,])))
     }
   }
   tableres<-data.frame(cbind(c,NA),stringsAsFactors = F)
   colnames(tableres)[(dim(tableres)[2]-1)]<-"times"
-  colnames(tableres)[1:(dim(tableres)[2]-2)]<-paste("Var",1:(dim(tableres)[2]-2),sep="")
+  colnames(tableres)[2:(dim(tableres)[2]-2)]<-paste("Var",2:(dim(tableres)[2]-2),sep="")
+  colnames(tableres)[1] <- "pval"
   colnames(tableres)[dim(tableres)[2]]<-"warnings"
-  colnames(solutions)[dim(solutions)[2]:(dim(solutions)[2]-1)]=c("swaps","p-value")
+  colnames(solutions)[dim(solutions)[2]:(dim(solutions)[2]-1)]=c("swaps","pval")
   withWarnings <- function(expr) {
     myWarnings <- NULL
     wHandler <- function(w) {
@@ -105,13 +106,13 @@ glmFSA=function(formula,data,fixvar=NULL,quad=F,m=2,numrs=1,save_solutions=T,fam
     val <- withCallingHandlers(expr, warning = wHandler)
     list(value = val, warnings = myWarnings)
   }
-    form<-function(j) formula(paste0(colnames(newdata)[1],"~",paste0(fixvar,sep="+"),paste(tableres[j,1:m],collapse = "*")),sep="")
-    warns<-NULL
+  form<-function(j) formula(paste0(colnames(newdata)[1],"~",paste0(fixvar,sep="+"),paste(tableres[j,2:m+1],collapse = "*")),sep="")
+  warns<-NULL
   for (i in 1:dim(tableres)[1]){
     ca<-as.character(withWarnings(glm(form(i),family=fam,data=newdata,...))$warnings[[1]])
     if(length(ca)==0){warns<-c(warns,NA)}
     else{warns<-c(warns,ca)}
-    }
+  }
   tableres$warnings<-warns
   return(list(solutions=solutions,table=tableres))
 }
