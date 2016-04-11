993e7f0657dcbf3fb2934d23dcf8f885925aba56
Joshua Lambert <jlambert@Joshuas-MacBook-Pro-4.local>
2016-04-11 16:04:02 -0400

    bug fixes

--- R/lmCriterion.R
+++ R/lmCriterion.R
@@ -1,28 +1,35 @@
+#' @export
 r.squared<-function(model){
   summary(model)$r.squared
 }
 
+#' @export
 adj.r.squared<-function(model){
   summary(model)$adj.r.squared
 }
 
+#' @export
 cv5.lmFSA<-function(model){
   as.numeric(tail(capture.output(cv.lm(data=dat,form.lm = model,m = 5,printit =T,plotit = F)),n=1))
 }
+
+#' @export
 cv10.lmFSA<-function(model){
   as.numeric(tail(capture.output(cv.lm(data=dat,form.lm = model,m = 10,printit =T,plotit = F)),n=1))
 }
 
+#' @export
 apress<-function(model){
   press(model)
 }
 
-
+#' @export
 which.max.na<-function(vec){
   maxval<-max(vec,na.rm=T)
   which(vec==maxval)
 }
 
+#' @export
 which.min.na<-function(vec){
   minval<-min(vec,na.rm=T)
   which(vec==minval)
