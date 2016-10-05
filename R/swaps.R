20ae55fab39a613f67aa264b92a4cda21770dfa2
Joshua Lambert <jmsuk0587@gmail.com>
2016-10-04 21:04:06 -0400

    Changing back before multicore fix.

--- R/swaps.R
+++ R/swaps.R
@@ -8,17 +8,24 @@
 #' @export
 swaps<- function(cur,n,quad=FALSE){
   m <- length(cur)
-  l <- (n-m)*m
-   possible<- do.call(rbind,parallel::mclapply(m:1,FUN=function(j){
-      possible <- matrix(rep(cur,l/m),nrow = l/m,byrow = T)
-      possible[,j]<-(1:n)[-which((1:n) %in% cur )]
-      possible
-    },mc.cores=m))
-  if(quad & m==2){
-   possible<-rbind(c(cur[1],cur[1]),possible,c(cur[2],cur[2]))
+  if(!quad){
+    l <- (n-m)*m
+    possible <- matrix(rep(cur,l),nrow = l,byrow = T)
+
+    s <- 1:n
+    lapply(1:m,FUN=function(i) s<<-s[-which(s==cur[i])])
+    lapply(1:m,FUN=function(j) possible[((j-1)*(n-m)+1):((j-1)*(n-m)+(n-m)),j] <<- s)
+  }
+  if(quad){
+    l <- (n-1)*m
+    possible <-  matrix(rep(cur,l),nrow = l,byrow = T)
+    s <- 1:n
+    smat <- matrix(rep(0,(n-1)*m),ncol=m)
+    lapply(1:m,FUN = function(i) smat[,i] <<- s[-which(s==cur[i])] )
+    lapply(1:m,FUN = function(j) possible[((j-1)*(n-1)+1):((j-1)*(n-1)+(n-1)),j] <<- smat[,j])
   }
   vec<-cbind(cur,t(possible))
-  return(unique(vec,MARGIN = 2))
+  return(unique(apply(vec,sort,MARGIN = 2),MARGIN = 2))
 }
 
 #' Variables to include in the >1st step of an mth order interaction model determined from the Feasible Soution Alorithm.
@@ -30,11 +37,12 @@ swaps<- function(cur,n,quad=FALSE){
 #' @param prevpos A vector of previous best spots
 #' @return a matrix with the possible forms by column.
 #' @export
-nextswap<-function(curpos,n,prevpos,quad=FALSE){
+nextswap<-function(curpos,n,prevpos,quad){
   swps<-swaps(curpos,n,quad)
-  nextpos<-colSums(do.call(rbind,parallel::mclapply(1:dim(swps)[1],FUN = function(qq){
-    (swps[qq,] %in% prevpos)
-  },mc.cores=dim(swps)[1])))
+  nextpos<-rep(FALSE,dim(swps)[2])
+  for(i in 1:dim(swps)[1]){
+    nextpos<-nextpos+(swps[i,] %in% prevpos)
+  }
   
   retSwps<-swps[,nextpos==(length(curpos)-2)]
   if(is.null(dim(retSwps))){retSwps<-t(t(retSwps))}
