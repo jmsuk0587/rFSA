6f36f2edbc538cf410fda615f481149faa64855d
Your Name <you@example.com>
2016-12-12 13:13:59 -0500

    Update to rFSA 0.1.0 (1st CRAN edition)

--- R/rstart.R
+++ R/rstart.R
@@ -4,13 +4,19 @@
 #' @param nvars number of varialbes that could be included in the interaction.
 #' @param quad whether to include quadratic terms or not.
 #' @param numrs number of random starts to compute.
+#' @importFrom graphics par plot
+#' @importFrom methods show
+#' @importFrom stats AIC anova as.formula cov fitted formula glm influence lm predict resid var
+#' @importFrom utils capture.output tail
 #'
 #' @return A matrix of random starts by row
 #' @export
 #'
 #' @examples
-#' rstart(2,5,quad=F,numrs=3) #doesn't include quadratic terms
-#' rstart(2,5,quad=T,numrs=3) #does include quadratic terms
-rstart=function(m,nvars,quad=F,numrs=1){# m is number of variables to include, nvars is a matrix of variables
-  t(replicate(numrs,sample(1:nvars,size=m,replace=quad)))
+#' rstart(2,5,quad=FALSE,numrs=3) #doesn't include quadratic terms
+#' rstart(2,5,quad=TRUE,numrs=3) #does include quadratic terms
+rstart = function(m,nvars,quad = FALSE,numrs = 1) {
+  t(replicate(numrs,sample(
+    1:nvars,size = m,replace = quad
+  )))
 }
